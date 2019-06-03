fdbb02f1d04f093f3e0388c79c96846368cb8b1a
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2019-06-03 19:58:19 +0200

    Added a help command, but not implemented yet Also added a check to see if the channel is correct Added a greet message (not tested)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # We import the discord.py library to help us with the API calls and such
 import discord
-
 # This is the part of the code which reads the users token from a file names token.
 def read_file():
     with open('token') as f:
@@ -12,5 +11,29 @@ def read_file():
 private_token = read_file()
 
 client = discord.Client()
+
+# This is dedicated to greeting users
+@client.event
+async def on_user_join(member):
+    # TODO: Make users be able to pick their own channels
+    greet_channel = ['welcome'] # This makes it so that users can only be greeted in the right channel
+    if str(member.guild.channel) in greet_channel:
+        await greet_channel.send(f"""Thank you to user {member.mention} for joining this server!""")
+
+
+@client.event
+async def on_message(msg):
+    server_id = client.get_guild(422531871314935810)
+    channels = ['commands', 'staff']
+    #Checks if the user sends help, in which we return the help menu
+    # TODO: Implement the help message
+    if str(msg.channel) in channels:
+        if msg.content.find('!!help') != -1:
+            await msg.channel.send('Hey, this is not implemented yet.')
+
+        elif msg.content == '!!members':
+            await msg.channel.send(f"""There are {server_id.member_count} members in this Discord server.""")
+
+
+
 client.run(private_token)
-#Yes
