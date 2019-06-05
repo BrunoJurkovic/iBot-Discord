5c51aa3694c8be655ef199961a6c9e2c47812910
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2019-06-05 20:44:15 +0200

    - Fixed some stuff

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -10,13 +10,21 @@ class Moderation(commands.Cog):
 
     @commands.command()
     async def kick(self, ctx, member : discord.Member, *, reason=None):
-        await member.kick(reason=reason)
-        await ctx.channel.send(f'''Successfully kicked {member.display_name} for the reason: `{reason}` ''')
+        try:
+            await member.kick(reason=reason)
+            await ctx.channel.send(f'''Successfully kicked {member.display_name} for the reason: `{reason}` ''')
+
+        except Exception as e:
+            await ctx.channel.send(f'''The user {member} could not be kicked. ''')
 
     @commands.command()
     async def ban(self, ctx, member : discord.Member, *, reason=None):
-        await member.ban(reason=reason)
-        await ctx.channel.send(f'''Successfully banned {member.display_name} for the reason: {reason} ''')
+        try:
+            await member.ban(reason=reason)
+            await ctx.channel.send(f'''Successfully banned {member.display_name} for the reason: {reason} ''')
+
+        except Exception as e:
+            await ctx.channel.send(f'''The user {member} could not be banned. ''')
 
     #TODO: Add unban
 
