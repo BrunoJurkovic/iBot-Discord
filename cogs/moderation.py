f7c5bbcc5c274f2da2cdf3aad5df77f72ef85ce8
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2019-06-07 19:12:29 +0200

    - Fixed the ping, again

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,28 +5,30 @@ import time
 from math import trunc
 from discord.ext import commands
 
+
 class Moderation(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     @commands.command()
-    async def kick(self, ctx, member : discord.Member, *, reason=None):
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
 
         if reason == None:
             reason = 'None'
             message = 'You have been kicked from the server for '
             if reason != 'None':
-               try:
-                message += reason
-                await member.send(message)
-               except:
-                   pass
+                try:
+                    message += reason
+                    await member.send(message)
+                except:
+                    pass
 
             else:
                 try:
-                 await member.send(message)
-                except: pass
+                    await member.send(message)
+                except:
+                    pass
         try:
             await member.kick(reason=reason)
             await ctx.channel.send(f'''Successfully kicked {member.display_name} for the reason: `{reason}` ''')
@@ -40,10 +42,42 @@ class Moderation(commands.Cog):
         em.add_field(name='Kick',
                      value=f'**User**: {member} [{member.id}]\n**Reason**: {reason}\n**Punisher**: {ctx.message.author}')
         await self.client.get_channel(int(log_channel)).send(embed=em)
-        #await self.punish("Kick", member, ctx.message.author, reason, 0) -- This is for when I will add the database
+        # await self.punish("Kick", member, ctx.message.author, reason, 0) -- This is for when I will add the database
 
     @commands.command()
-    async def ban(self, ctx, member : discord.Member, *, reason=None):
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+
+        if not reason:
+            reason = 'None'
+            message = 'You have been banned from the server for '
+            if reason != 'None':
+                message += reason
+                await member.send(message)
+
+            else:
+                await member.send(message)
+
+        try:
+            await member.ban(reason=reason)
+            await ctx.channel.send(f'''Successfully banned {member.display_name} for the reason: {reason} ''')
+
+        except Exception as e:
+            await ctx.channel.send(f'''The user {member} could not be banned. ''')
+
+    @commands.command()
+    async def ping(self, ctx):
+        current_time = time.perf_counter()
+        await ctx.channel.send(".")
+        time_after = time.perf_counter()
+
+        await ctx.channel.purge(limit=1)
+
+        final_time = time_after - current_time
+
+        await ctx.channel.send(f'''Latancy is: `{round(final_time * 1000)}`ms''')
+
+    @commands.command()
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
 
         if not reason:
             reason = 'None'
@@ -94,7 +128,7 @@ class Moderation(commands.Cog):
         await self.client.get_channel(int(log_channel)).send(embed=em)
         # await self.punish("UnBan", member, ctx.message.author, reason, 0) -- This is for when I will add the database
 
-    #TODO Add checks
+    # TODO Add checks
 
     @commands.command()
     async def clear(self, ctx, limit):
