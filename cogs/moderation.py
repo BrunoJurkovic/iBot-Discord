cc907210738b3f9c6763285092bebb83d369b9c3
chris <lijschristian@gmail.com>
2019-06-08 11:38:49 +0100

    made a temporary error handler for the ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -43,38 +43,6 @@ class Moderation(commands.Cog):
                      value=f'**User**: {member} [{member.id}]\n**Reason**: {reason}\n**Punisher**: {ctx.message.author}')
         await self.client.get_channel(int(log_channel)).send(embed=em)
         # await self.punish("Kick", member, ctx.message.author, reason, 0) -- This is for when I will add the database
-
-    @commands.command()
-    async def ban(self, ctx, member: discord.Member, *, reason=None):
-
-        if not reason:
-            reason = 'None'
-            message = 'You have been banned from the server for '
-            if reason != 'None':
-                message += reason
-                await member.send(message)
-
-            else:
-                await member.send(message)
-
-        try:
-            await member.ban(reason=reason)
-            await ctx.channel.send(f'''Successfully banned {member.display_name} for the reason: {reason} ''')
-
-        except Exception as e:
-            await ctx.channel.send(f'''The user {member} could not be banned. ''')
-
-    @commands.command()
-    async def ping(self, ctx):
-        current_time = time.perf_counter()
-        await ctx.channel.send(".")
-        time_after = time.perf_counter()
-
-        await ctx.channel.purge(limit=1)
-
-        final_time = time_after - current_time
-
-        await ctx.channel.send(f'''Latancy is: `{round(final_time * 1000)}`ms''')
  
     @commands.command()
     async def ping(self, ctx): # ik it's bad code
@@ -91,29 +59,33 @@ class Moderation(commands.Cog):
     @commands.command()
     async def ban(self, ctx, member : discord.Member, *, reason=None):
 
-        try:
-
-            if (reason == None):
-                await ctx.channel.send(f'''Successfully banned {member.display_name}''')
-                message = f'''You have been banned from the server {member.guild.name}'''
-                await member.send(message)
-            else:
-                await ctx.channel.send(f'''Successfully banned {member.display_name} for reason: {reason} ''')
-                message = f'''You have been banned from the server {member.guild.name} for {reason}'''
-                await member.send(message)
-
+        if (reason == None):
+            await ctx.channel.send(f'''Successfully banned {member.display_name}''')
+            message = f'''You have been banned from the server {member.guild.name}'''
+            await member.send(message)
             await member.ban(reason=reason)
-
-        except Exception:
-            await ctx.channel.send(f'''The user {member} could not be banned. ''')
+        else:
+            await ctx.channel.send(f'''Successfully banned {member.display_name} for reason: {reason} ''')
+            message = f'''You have been banned from the server {member.guild.name} for {reason}'''
+            await member.send(message)
+            await member.ban(reason=reason)
 
         log_channel = 422534466334883850
         em = discord.Embed(color=discord.Color.dark_purple())
         em.set_footer().timestamp = datetime.datetime.utcnow()
         em.add_field(name='Ban',
-                     value=f'**User**: {member} [{member.id}]\n**Reason**: {reason}\n**Punisher**: {ctx.message.author}')
+                    value=f'**User**: {member} [{member.id}]\n**Reason**: {reason}\n**Punisher**: {ctx.message.author}')
         await self.client.get_channel(int(log_channel)).send(embed=em)
         # await self.punish("Ban", member, ctx.message.author, reason, 0) -- This is for when I will add the database
+
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, discord.Forbidden):
+            await ctx.channel.send(f'''Error, you don't have permission to ban''')
+        elif isinstance(error, discord.HTTPException):
+            await ctx.channel.send(f'''Error, banning user failed''')
+        else:
+            await ctx.channel.send(f'''Error, user is not on the server''')
 
     @commands.command()
     async def unban(self, ctx, *, member, reason=None):
